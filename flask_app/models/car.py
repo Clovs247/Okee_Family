92d9e27a63bec8fa6b740a73f6619d86df054b79
David Hausmann <david_hausmann@aol.com>
2022-12-12 12:58:35 -0500

    updated car model

--- flask_app/models/car.py
+++ flask_app/models/car.py
@@ -1,5 +1,6 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
+from flask_app.models import user
 
 
 class Car:
@@ -71,15 +72,48 @@ class Car:
             car_data = {
                 'car_id' : car_now.id
             }
-            car_now.passengers = cls.get_voyagers(car_data)
+            car_now.passengers = cls.get_passengers(car_data)
             voyagers.append(car_now)
             return cls(results[0])
     
     @classmethod
     def get_car_by_driver(cls, data):
         pass
+    
+    @classmethod
+    def get_passengers(cls, data):
+        query="""
+        SELECT * FROM passenger
+        JOIN user
+        ON passenger.user_id = user.id
+        WHERE passenger.car_id = %(car_id)s
+        ;"""
+        results = connectToMySQL(cls.db).query_db(query, data)
+        voyagers = []
+        for row in results:
+            voyagers.append(user.User(row))
+        return voyagers
 # &&&&&&&&&&&&&&&&&&&&&&&&&&&&&& Update &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
-# &&&&&&&&&&&&&&&&&&&&&&&&&&&&&& Create &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+# &&&&&&&&&&&&&&&&&&&&&&&&&&&&&& Delete &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
-# &&&&&&&&&&&&&&&&&&&&&&&&&&&&&& Create &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+# &&&&&&&&&&&&&&&&&&&&&&&&&&&&&& Validate &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+    @staticmethod
+    def validate_car(car_data):
+        is_valid = True
+        query="""
+        SELECT * FROM car
+        WHERE car_name = %(car_name)s
+        ;"""
+        results = connectToMySQL(Car.db).query_db(query, car_data)
+        if len(car_data['car_name']) <= 3:
+            is_valid = False
+            flash("Please name your car with at least 3 characters, so we know which car is which.")
+        if car_data["car_capacity"] <=0:
+            is_valid=False
+            flash("Its not a ghost car, how many people fit in it?")
+        if car_data["car_capacity"] >10:
+            is_valid=False
+            flash("ARE YOU DRIVING A BUS?!")
+        return is_valid
